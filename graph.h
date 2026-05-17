62f03d70b93b5bc83a6c9d91303374c6511e2b49
Laura <laura.lukianaite@gmail.com>
2026-05-17 14:13:17 +0300

    Initialize graph structures and headers

---
+++ graph.h
@@ -0,0 +1,21 @@
+#ifndef GRAFAS_H
+#define GRAFAS_H
+
+#define MAX_CITIES 100
+#define MAX_NAME_LEN 50
+#define INF 999999
+
+typedef struct {
+    char name[MAX_NAME_LEN];
+} City;
+
+typedef struct {
+    City cities[MAX_CITIES];
+    int city_count;
+    int adj_matrix[MAX_CITIES][MAX_CITIES];
+} Graph;
+
+void init_graph(Graph *g);
+int get_city_index(Graph *g, char *name);
+
+#endif
